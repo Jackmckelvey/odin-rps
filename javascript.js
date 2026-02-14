51cd5e981b437488cee7de19a9bb3cffa5db3bb7
Jackmckelvey <jack.mckelvey454@gmail.com>
2026-02-14 14:23:16 -0800

    finish product

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//slecting divs
+//selecting divs
 
 const scoreP = document.querySelector(".scoreP");
 const result = document.querySelector(".results");
@@ -6,6 +6,9 @@ const resultP =document.querySelector(".resultP");
 const buttonR = document.querySelector(".rock");
 const buttonP = document.querySelector(".paper");
 const buttonS = document.querySelector(".scissors");
+
+let score = 0;
+let game = true;
 
 function getComputerChoice(){
     let cChoice = Math.floor(Math.random() * 3);
@@ -20,25 +23,36 @@ function getComputerChoice(){
     }
 }
 
+function checkWin(){
+    if (score === 5){
+        resultP.textContent = "You Win The Game";
+        game = false;
+    }
+}
 
-function playRound(human, computer) {
-    if (human=="rock" && computer=="scissors" ||
-        human=="scissors" && computer=="paper" ||
-        human=="paper" && computer=="rock"){
-        resultP.textContent = "You Won!";
-        score++;
-        scoreP.textContent = score;
-        result.style.backgroundcolor = "Green";
+
+function playRound(human, computer, game) {
+    if (game){
+        if (human=="rock" && computer=="scissors" ||
+            human=="scissors" && computer=="paper" ||
+            human=="paper" && computer=="rock"){
+            resultP.textContent = "You Won!";
+            score++;
+            scoreP.textContent = score;
+            result.style.backgroundColor = "green";
+        }
+        else if (computer=="rock" && human=="scissors" ||
+            computer=="scissors" && human=="paper" ||
+            computer=="paper" && human=="rock"){
+            resultP.textContent = "You Lost!";
+            result.style.backgroundColor = "red";
+        }
+        else{
+            resultP.textContent = "It was a tie!";
+            result.style.backgroundColor = "white";
+        }
     }
-    else if (computer=="rock" && human=="scissors" ||
-        computer=="scissors" && human=="paper" ||
-        computer=="paper" && human=="rock"){
-        resultP.textContent = "You Lost!";
-        result.style.backgroundcolor = "Red";
-    }
-    else{
-        resultP.textContent = "It was a tie!";
-    }
+    checkWin();
 }
 
 /*
@@ -49,6 +63,6 @@ buttons.forEach((button) => {
 });
 */
 
-buttonR.onclick = playRound("rock", getComputerChoice());
-buttonP.onclick = playRound("paper", getComputerChoice());
-buttonS.onclick = playRound("scissors", getComputerChoice());
+buttonR.onclick = () => playRound("rock", getComputerChoice(), game);
+buttonP.onclick = () => playRound("paper", getComputerChoice(), game);
+buttonS.onclick = () => playRound("scissors", getComputerChoice(), game);
